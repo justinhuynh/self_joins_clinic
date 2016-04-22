3b382a1efe437bc2803c2ec88ec150e7596e3604
justinhuynh <jhuynh@gmail.com>
2016-04-22 14:05:08 -0400

    seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 poro = User.create(name: "Poro")
 fluffy = User.create(name: "Fluffy")
 whiskers = User.create(name: "Whiskers")
@@ -15,3 +17,21 @@ fluffy.manager = monster
 whiskers.manager = monster
 
 monster.manager = sloth
+
+# Non-standard many-to-many relationships
+
+# Item has a `seller` and `broker` - both users
+5.times do
+  Item.create(
+    description: Faker::Hipster.sentence(4),
+    seller: gilly,
+    broker: [gilly, monster].sample
+  )
+end
+
+30.times do
+  PurchasedItem.create(
+    buyer: [monster, parrot, gilly, whiskers, sloth].sample
+    item: Item.all.sample
+  )
+end
